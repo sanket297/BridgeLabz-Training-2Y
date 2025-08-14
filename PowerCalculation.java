e485af29b9198ff8a7f55293257d21338c875b77
sanket297 <sanketesingh9757@gmail.com>
2025-08-14 15:55:56 +0530

    done

---
+++ PowerCalculation.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class PowerCalculation {
+    public static void main(String[] args) {
+        double base = sc.nextDouble();
+        double exponent = sc.nextDouble();
+        double result = Math.pow(base, exponent);
+        System.out.println(result);
+    }
+}
